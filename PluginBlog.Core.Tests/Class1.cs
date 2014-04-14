cf9a2c31021ad06316f1fd403b148aaa401fb996
Giorgio <george2giga@gmail.com>
2014-04-14 02:03:00 +0100

    * commenting tests

--- PluginBlog.Core.Tests/Class1.cs
+++ PluginBlog.Core.Tests/Class1.cs
@@ -25,159 +25,159 @@ namespace PluginBlog.Core.Tests
         //    context.Database.Initialize(false);
         //}
 
-        [Test]
-        public void GetAllPosts()
-        {
-            var blogRepo = new BlogRepository(new BlogContext());
-            var posts = blogRepo.AllPosts().ToList();
-            string e = "";
+        //[Test]
+        //public void GetAllPosts()
+        //{
+        //    var blogRepo = new BlogRepository(new BlogContext());
+        //    var posts = blogRepo.AllPosts().ToList();
+        //    string e = "";
 
-        }
+        //}
 
-        [Test]
-        public void GetOnePost()
-        {
-            var blogRepo = new BlogRepository(new BlogContext());
-            var posts = blogRepo.Post(1);
-            string e = "";
+        //[Test]
+        //public void GetOnePost()
+        //{
+        //    var blogRepo = new BlogRepository(new BlogContext());
+        //    var posts = blogRepo.Post(1);
+        //    string e = "";
 
-        }
+        //}
 
-        [Test]
-        public void GetAllTags()
-        {
-            var blogRepo = new BlogRepository(new BlogContext());
-            var tags = blogRepo.Tags().OrderByDescending(x => x.Id).ToList();
-            string e = "";
+        //[Test]
+        //public void GetAllTags()
+        //{
+        //    var blogRepo = new BlogRepository(new BlogContext());
+        //    var tags = blogRepo.Tags().OrderByDescending(x => x.Id).ToList();
+        //    string e = "";
 
-        }
+        //}
 
-        [Test]
-        public void UpdateOneCategory()
-        {
-            var context = new BlogContext();
-            var post = context.Find<Post>(16);
-            post.Category = context.Find<Category>(2);
-            //var blogRepo = new BlogRepository(new BlogContext());
-            //var post = blogRepo.Post(16);
-            //post.Category = blogRepo.Category(1);
-            //blogRepo.EditPost(post);
-            context.Entry(post).State = EntityState.Modified;
-            context.SaveChanges();
-        }
+        //[Test]
+        //public void UpdateOneCategory()
+        //{
+        //    var context = new BlogContext();
+        //    var post = context.Find<Post>(16);
+        //    post.Category = context.Find<Category>(2);
+        //    //var blogRepo = new BlogRepository(new BlogContext());
+        //    //var post = blogRepo.Post(16);
+        //    //post.Category = blogRepo.Category(1);
+        //    //blogRepo.EditPost(post);
+        //    context.Entry(post).State = EntityState.Modified;
+        //    context.SaveChanges();
+        //}
 
-        [Test]
-        public void UpdateOneCategoryNewPost()
-        {
-            var context = new BlogContext();
+        //[Test]
+        //public void UpdateOneCategoryNewPost()
+        //{
+        //    var context = new BlogContext();
             
-            var Newpost = new Post()
-                       {
-                           Id = 1,
-                           LastModified = DateTime.Now,
-                           PostedOn = DateTime.Now,
-                           Title = "sosdkfosf",
-                           ShortDescription = "isodjfsfjisofj",
-                           Description = "isodjfsfjisofj",
-                           UrlSlug = "sdfsfs",
-                           Meta = "sdfsfs",
-                           Published = false,
-                           CategoryId = 1//, Tags = new Collection<Tag>()
-                       };
-            //Newpost.Category = context.Categories.Find(1);
+        //    var Newpost = new Post()
+        //               {
+        //                   Id = 1,
+        //                   LastModified = DateTime.Now,
+        //                   PostedOn = DateTime.Now,
+        //                   Title = "sosdkfosf",
+        //                   ShortDescription = "isodjfsfjisofj",
+        //                   Description = "isodjfsfjisofj",
+        //                   UrlSlug = "sdfsfs",
+        //                   Meta = "sdfsfs",
+        //                   Published = false,
+        //                   CategoryId = 1//, Tags = new Collection<Tag>()
+        //               };
+        //    //Newpost.Category = context.Categories.Find(1);
 
-            //context.Entry(Newpost.Tags).State = EntityState.Modified;
-            context.Entry(Newpost).State = EntityState.Modified;
-            context.Entry(Newpost).Reload();
-            //Newpost.Tags.Clear();
-            //var tag = context.Posts.Find(1).Tags;
-            //Newpost.Tags.Add(tag);
+        //    //context.Entry(Newpost.Tags).State = EntityState.Modified;
+        //    context.Entry(Newpost).State = EntityState.Modified;
+        //    context.Entry(Newpost).Reload();
+        //    //Newpost.Tags.Clear();
+        //    //var tag = context.Posts.Find(1).Tags;
+        //    //Newpost.Tags.Add(tag);
             
 
-            Newpost.Tags.Clear();
+        //    Newpost.Tags.Clear();
 
             
-            try
-            {
-                context.SaveChanges();
-            }
-            catch (System.Data.Entity.Validation.DbEntityValidationException dbEx)
-            {
-                Exception raise = dbEx;
-                foreach (var validationErrors in dbEx.EntityValidationErrors)
-                {
-                    foreach (var validationError in validationErrors.ValidationErrors)
-                    {
-                        string message = string.Format("{0}:{1}",
-                            validationErrors.Entry.Entity.ToString(),
-                            validationError.ErrorMessage);
-                        // raise a new exception nesting
-                        // the current instance as InnerException
-                        raise = new InvalidOperationException(message, raise);
-                    }
-                }
-                throw raise;
-            }
+        //    try
+        //    {
+        //        context.SaveChanges();
+        //    }
+        //    catch (System.Data.Entity.Validation.DbEntityValidationException dbEx)
+        //    {
+        //        Exception raise = dbEx;
+        //        foreach (var validationErrors in dbEx.EntityValidationErrors)
+        //        {
+        //            foreach (var validationError in validationErrors.ValidationErrors)
+        //            {
+        //                string message = string.Format("{0}:{1}",
+        //                    validationErrors.Entry.Entity.ToString(),
+        //                    validationError.ErrorMessage);
+        //                // raise a new exception nesting
+        //                // the current instance as InnerException
+        //                raise = new InvalidOperationException(message, raise);
+        //            }
+        //        }
+        //        throw raise;
+        //    }
 
-        }
+        //}
 
-        [Test]
-        public void UpdateOneCategoryNewPostBis()
-        {
-            var context = new BlogContext();
+        //[Test]
+        //public void UpdateOneCategoryNewPostBis()
+        //{
+        //    var context = new BlogContext();
 
-            var Newpost = new Post()
-            {
-                LastModified = DateTime.Now,
-                PostedOn = DateTime.Now,
-                Title = "sosdkfosf",
-                ShortDescription = "isodjfsfjisofj",
-                Description = "isodjfsfjisofj",
-                UrlSlug = "sdfsfs",
-                Meta = "sdfsfs",
-                Published = false,
-                CategoryId = 2
-            };
+        //    var Newpost = new Post()
+        //    {
+        //        LastModified = DateTime.Now,
+        //        PostedOn = DateTime.Now,
+        //        Title = "sosdkfosf",
+        //        ShortDescription = "isodjfsfjisofj",
+        //        Description = "isodjfsfjisofj",
+        //        UrlSlug = "sdfsfs",
+        //        Meta = "sdfsfs",
+        //        Published = false,
+        //        CategoryId = 2
+        //    };
 
-            var post = context.Posts.SingleOrDefault(x=>x.Id == 1);
-            post.CategoryId = Newpost.CategoryId;
-            var firstTag = context.Find<Tag>(4);
-            if(!post.Tags.Any(x=>x.Id == firstTag.Id))
-                post.Tags.Add(firstTag);
+        //    var post = context.Posts.SingleOrDefault(x=>x.Id == 1);
+        //    post.CategoryId = Newpost.CategoryId;
+        //    var firstTag = context.Find<Tag>(4);
+        //    if(!post.Tags.Any(x=>x.Id == firstTag.Id))
+        //        post.Tags.Add(firstTag);
 
-            //Newpost.Category = context.Categories.Find(1);
-            //context.Entry(Newpost.Tags).State = EntityState.Modified;
-            //context.Entry(Newpost).State = EntityState.Modified;
-            //Newpost.Tags.Clear();
-            //var tag = context.Posts.Find(1).Tags;
-            //Newpost.Tags.Add(tag);
-
-
-            //Newpost.Tags.Clear();
+        //    //Newpost.Category = context.Categories.Find(1);
+        //    //context.Entry(Newpost.Tags).State = EntityState.Modified;
+        //    //context.Entry(Newpost).State = EntityState.Modified;
+        //    //Newpost.Tags.Clear();
+        //    //var tag = context.Posts.Find(1).Tags;
+        //    //Newpost.Tags.Add(tag);
 
 
-            try
-            {
-                context.SaveChanges();
-            }
-            catch (System.Data.Entity.Validation.DbEntityValidationException dbEx)
-            {
-                Exception raise = dbEx;
-                foreach (var validationErrors in dbEx.EntityValidationErrors)
-                {
-                    foreach (var validationError in validationErrors.ValidationErrors)
-                    {
-                        string message = string.Format("{0}:{1}",
-                            validationErrors.Entry.Entity.ToString(),
-                            validationError.ErrorMessage);
-                        // raise a new exception nesting
-                        // the current instance as InnerException
-                        raise = new InvalidOperationException(message, raise);
-                    }
-                }
-                throw raise;
-            }
+        //    //Newpost.Tags.Clear();
 
-        }
+
+        //    try
+        //    {
+        //        context.SaveChanges();
+        //    }
+        //    catch (System.Data.Entity.Validation.DbEntityValidationException dbEx)
+        //    {
+        //        Exception raise = dbEx;
+        //        foreach (var validationErrors in dbEx.EntityValidationErrors)
+        //        {
+        //            foreach (var validationError in validationErrors.ValidationErrors)
+        //            {
+        //                string message = string.Format("{0}:{1}",
+        //                    validationErrors.Entry.Entity.ToString(),
+        //                    validationError.ErrorMessage);
+        //                // raise a new exception nesting
+        //                // the current instance as InnerException
+        //                raise = new InvalidOperationException(message, raise);
+        //            }
+        //        }
+        //        throw raise;
+        //    }
+
+        //}
     }
 }
