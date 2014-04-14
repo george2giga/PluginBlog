3a1f6a252de8fb144af5d43214544b11c2650dfb
Giorgio <george2giga@gmail.com>
2014-04-14 15:07:21 +0100

    * added auth

---
+++ PluginBlog.Web/Areas/BlogAdmin/Controllers/AuthController.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using PluginBlog.Web.Areas.BlogAdmin.Models;
+using PluginBlog.Web.Helpers;
+
+namespace PluginBlog.Web.Areas.BlogAdmin.Controllers
+{
+    public class AuthController : Controller
+    {
+        private readonly IAuthProvider _authProvider;
+
+        public AuthController()
+        {
+            _authProvider = PluginBlogConfig.GetAuthorization();
+        }
+
+        public AuthController(IAuthProvider authProvider)
+        {
+            _authProvider = authProvider;
+        }
+
+        public ActionResult Login()
+        {
+            var model = new LoginModel();
+            return View(model);
+        }
+
+        public ActionResult Logout()
+        {
+            _authProvider.Logout();
+            return Redirect("~/");
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Login(LoginModel model)
+        {
+            if (ModelState.IsValid)
+            {
+                var login = _authProvider.Login(model.Username, model.Password);
+                if (login)
+                {
+                    return RedirectToAction("Index", "Post");
+                }
+            }
+            return View(model);
+        }
+	}
+}
---
+++ PluginBlog.Web/Helpers/AuthProvider.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Security;
+
+namespace PluginBlog.Web.Helpers
+{
+    public class SampleAuthProvider : IAuthProvider
+    {
+        /// <summary>
+        /// Return True if the user is already logged-in.
+        /// </summary>
+        public bool IsLoggedIn
+        {
+            get
+            {
+                return HttpContext.Current.User.Identity.IsAuthenticated;
+            }
+        }
+
+        /// <summary>
+        /// Authenticate an user and set cookie if user is valid.
+        /// </summary>
+        /// <param name="username"></param>
+        /// <param name="password"></param>
+        /// <returns></returns>
+        public bool Login(string username, string password)
+        {
+            
+            var result = FormsAuthentication.Authenticate(username, password); // TODO: User Membership APIs
+
+            if (result)
+                FormsAuthentication.SetAuthCookie(username, false);
+
+            return result;
+        }
+
+        /// <summary>
+        /// Logout the user.
+        /// </summary>
+        public void Logout()
+        {
+            FormsAuthentication.SignOut();
+        }
+    }
+}
